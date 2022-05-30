e1c4503a9cb11e027c325c3297dde405037345c2
rushi2784 <96285307+rushi2784@users.noreply.github.com>
2022-05-30 15:31:46 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,7 +1,15 @@
 import React from "react";
-
+import Products from "./components/Products";
+import { Container } from "@chakra-ui/react";
 const App = () => {
-  return <div>{/* TODO: Code here */}</div>;
+  return <div>{/* TODO: Code here */
+  <Container maxW="container.xl" textAlign="center">
+        <Products />
+     
+      </Container>
+
+ 
+  }</div>;
 };
 
 export default App;
